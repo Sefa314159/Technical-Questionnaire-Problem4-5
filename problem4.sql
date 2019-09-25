6c864ba116b8babf926616669fe81a56c47539e0
Sefa İşci <sefa314159@gmail.com>
2019-09-25 22:30:09 +0300

    Create problem4.sql

---
+++ problem4.sql
@@ -0,0 +1,5 @@
+SELECT username, emailaddress FROM
+(SELECT i_users.userld, i_users.username, i_users.emailaddress, COUNT(i_user_login_logs.userld) as logins
+ FROM i_users LEFT JOIN i_user_login_logs ON i_users.userld = i_user_login_logs.userld WHERE NOT username = 'Administrator'
+ GROUP BY i_user_login_logs.userld ORDER BY logins DESC)
+WHERE logins > 3
